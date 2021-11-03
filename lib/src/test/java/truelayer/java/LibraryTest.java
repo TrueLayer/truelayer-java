4b4b115913cd98d8052d67c4a6c1265e55af2fa3
giulio.leso <giulio.leso@truelayer.com>
2021-11-03 15:38:31 +0100

    working signing library

--- lib/src/test/java/truelayer/java/LibraryTest.java
+++ lib/src/test/java/truelayer/java/LibraryTest.java
@@ -4,11 +4,53 @@
 package truelayer.java;
 
 import org.junit.Test;
+import truelayer.signing.Signer;
+import truelayer.signing.Verifier;
+
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+
+    static String KID = "45fc75cf-5649-4134-84b3-192c2c78e990";
+
+    @Test
+    public void fullSignature() throws IOException {
+        byte[] privateKey = Files.readAllBytes(Path.of("src/test/resources/ec512-private-key.pem"));
+        byte[] publicKey = Files.readAllBytes(Path.of("src/test/resources/ec512-public-key.pem"));
+
+        byte[] body = "{\"currency\":\"GBP\",\"max_amount_in_minor\":5000000}".getBytes(StandardCharsets.UTF_8);
+        String idempotencyKey = "idemp-2076717c-9005-4811-a321-9e0787fa0382";
+        String path = "/merchant_accounts/a61acaef-ee05-4077-92f3-25543a11bd8d/sweeping";
+
+        String tlSignature = Signer.from(KID, privateKey)
+                .header("Idempotency-Key", idempotencyKey)
+                .method("post")
+                .path(path)
+                .body(body)
+                .sign();
+
+        boolean verified = Verifier.from(publicKey)
+                .method("POST")
+                .path(path)
+                .header("X-Whatever", "aoitbeh")
+                .header("Idempotency-Key", idempotencyKey)
+                .body(body)
+                .requiredHeader("Idempotency-Key")
+                .verify(tlSignature);
+
+        assertTrue(verified);
+    }
+
+
+    @Test
+    public void stupidTest() throws IOException {
+        assertTrue(true);
     }
 }
+
