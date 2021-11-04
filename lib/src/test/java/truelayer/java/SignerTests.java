020bd9a2b0999753a27aaf0f5df93cf58890e8b7
Luca Baggi <74901945+tl-luca-baggi@users.noreply.github.com>
2021-11-04 14:49:27 +0100

    Header signature implementation (#10)
    * first impl
    
    * test to create a payment and retrieve it
    
    * test to create a payment and retrieve it
    
    * test to create a payment and retrieve it
    
    * added bouncy castle dependency
    
    * payments stub
    
    * clean up
    
    * added verifier + impr
    
    Co-authored-by: giulio.leso <giulio.leso@truelayer.com>

---
+++ lib/src/test/java/truelayer/java/SignerTests.java
@@ -0,0 +1,34 @@
+package truelayer.java;
+
+import com.nimbusds.jose.JOSEException;
+import org.junit.jupiter.api.Test;
+import truelayer.java.signing.Signer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.ParseException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SignerTests {
+
+    private static final String A_PAYLOAD = "{\"foo\":\"bar\"}";
+    private static final String A_KEY_ID = "a_key_id";
+    private static final String A_METHOD = "POST";
+    private static final String A_PATH = "/test";
+    private static final Map<String, String> headers = Map.of("Idempotency-Key", "idemp_key");
+
+    @Test
+    public void SignerShouldCreateValidSignature() throws JOSEException, IOException, ParseException {
+        var signatureBuilder = new Signer.Builder(A_KEY_ID, Files.readAllBytes(Path.of("src/test/resources/ec512-private-key.pem")))
+                .addHttpMethod(A_METHOD)
+                .addPath(A_PATH)
+                .addBody(A_PAYLOAD);
+        headers.forEach(signatureBuilder::addHeader);
+        var signature = signatureBuilder.getSignature();
+        System.out.println(signature);
+        assertNotNull(signature);
+    }
+}
