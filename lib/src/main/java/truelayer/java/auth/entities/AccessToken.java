04b1ac52b317a19ca273b8660ac4fb42af1eccce
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-04 10:23:16 +0100

    syntactic sugar

--- lib/src/main/java/truelayer/java/auth/entities/AccessToken.java
+++ lib/src/main/java/truelayer/java/auth/entities/AccessToken.java
@@ -1,38 +1,16 @@
 package truelayer.java.auth.entities;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Value;
 
+@Value
 public class AccessToken {
-
     @SerializedName("access_token")
-    private final String accessToken;
+    String accessToken;
     @SerializedName("expires_in")
-    private final int expiresIn;
+    int expiresIn;
     @SerializedName("scope")
-    private final String scope;
+    String scope;
     @SerializedName("token_type")
-    private final String tokenType;
-
-    public AccessToken(String accessToken, int expiresIn, String scope, String tokenType) {
-        this.accessToken = accessToken;
-        this.expiresIn = expiresIn;
-        this.scope = scope;
-        this.tokenType = tokenType;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public int getExpiresIn() {
-        return expiresIn;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
+    String tokenType;
 }
