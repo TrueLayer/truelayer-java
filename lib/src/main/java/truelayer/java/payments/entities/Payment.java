c7a99677b051ed860b73c7c5c9e714339ffe9156
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-04 18:15:45 +0100

    added jackson instruction to ignore unknown fields

--- lib/src/main/java/truelayer/java/payments/entities/Payment.java
+++ lib/src/main/java/truelayer/java/payments/entities/Payment.java
@@ -1,9 +1,11 @@
 package truelayer.java.payments.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
+@JsonIgnoreProperties(ignoreUnknown = true) //todo remove and complete the DTO
 public class Payment {
 
     @JsonProperty("id")
