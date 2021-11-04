ab1afee8ddf864a00437ad5da5430c52ab96616b
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-04 18:08:14 +0100

    cleanup

--- lib/src/main/java/truelayer/java/payments/entities/Payment.java
+++ lib/src/main/java/truelayer/java/payments/entities/Payment.java
@@ -1,15 +1,9 @@
 package truelayer.java.payments.entities;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Value;
 
-@Getter
-@Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Value
 public class Payment {
 
     @JsonProperty("id")
@@ -17,10 +11,4 @@ public class Payment {
 
     @JsonProperty("status")
     private final String status;
-
-    @JsonCreator
-    public Payment(@JsonProperty("id") String paymentId, @JsonProperty("status") String status) {
-        this.paymentId = paymentId;
-        this.status = status;
-    }
 }
