0d77b8cdfe792fff2ae66319e79da5fbc8e380df
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-16 20:10:18 +0100

    first payment api interface defined

---
+++ lib/src/main/java/truelayer/java/payments/IPaymentsApi.java
@@ -0,0 +1,18 @@
+package truelayer.java.payments;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import truelayer.java.payments.entities.CreatePaymentRequest;
+import truelayer.java.payments.entities.Payment;
+
+public interface IPaymentsApi {
+
+    @POST("/payments")
+    Call<Payment> createPayment(
+            @Header("Idempotency-Key") String idempotencyKey,
+            @Header("Signature") String signature,
+            @Header("Authorization") String authorization,
+            @Body CreatePaymentRequest body);
+}
