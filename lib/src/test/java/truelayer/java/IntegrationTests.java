22553b0d128c7a8b67f731d444ce4089a68be687
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-16 11:25:48 +0100

    dynamic wiremock port

--- lib/src/test/java/truelayer/java/IntegrationTests.java
+++ lib/src/test/java/truelayer/java/IntegrationTests.java
@@ -1,23 +1,40 @@
 package truelayer.java;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.apache.commons.lang3.reflect.FieldUtils.getField;
+import static org.apache.commons.lang3.reflect.FieldUtils.writeField;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@WireMockTest(httpPort = 9000) //todo: try to randomize this
+
+@WireMockTest
 public class IntegrationTests {
+
+    TrueLayerClient tlClient;
+
+    @SneakyThrows
+    @BeforeEach
+    public void setup(WireMockRuntimeInfo wireMockRuntimeInfo){
+        tlClient = TrueLayerClient.builder()
+                .clientCredentialsOptions(TestUtils.getClientCredentialsOptions())
+                .build();
+
+        writeField(tlClient, "endpointUrl", wireMockRuntimeInfo.getHttpBaseUrl(), true);
+    }
 
     @Test
     public void shouldReturnAnUnauthorizedError(){
         stubFor(post("/connect/token").willReturn(unauthorized()));
-        var tlClient = TrueLayerClient.builder()
-                .clientCredentialsOptions(TestUtils.getClientCredentialsOptions())
-                .build();
 
         var thrown = Assertions.assertThrows(TrueLayerException.class,
                 ()-> tlClient.auth().getOauthToken(List.of("paydirect")));
