d4cd9381dd75ba38e5ee5d536a7d7528c3f29727
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2021-11-19 16:44:25 +0100

    oversights fixed again

--- lib/src/main/java/truelayer/java/TrueLayerClient.java
+++ lib/src/main/java/truelayer/java/TrueLayerClient.java
@@ -102,9 +102,9 @@ public class TrueLayerClient implements ITrueLayerClient {
 
     public static class ConfigurationKeys {
         public static final String AUTH_ENDPOINT_URL_LIVE = "tl.auth.endpoint.live";
-        public static final String AUTH_ENDPOINT_URL_SANDBOX = "tl.auth.endpoint.live";
+        public static final String AUTH_ENDPOINT_URL_SANDBOX = "tl.auth.endpoint.sandbox";
 
-        public static final String PAYMENTS_ENDPOINT_URL_LIVE = "tl.payments.endpoint.sandbox";
+        public static final String PAYMENTS_ENDPOINT_URL_LIVE = "tl.payments.endpoint.live";
         public static final String PAYMENTS_ENDPOINT_URL_SANDBOX = "tl.payments.endpoint.sandbox";
         public static final String PAYMENTS_SCOPES = "tl.payments.scopes";
     }
