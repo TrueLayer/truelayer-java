caaa6fd7c7fbb88cd617d43bb89b4e051ee33357
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-01-18 15:36:31 +0100

    solving linting error

--- lib/src/main/java/truelayer/java/payments/entities/paymentdetail/AuthorizationRequiredPaymentDetail.java
+++ lib/src/main/java/truelayer/java/payments/entities/paymentdetail/AuthorizationRequiredPaymentDetail.java
@@ -4,7 +4,6 @@ import static truelayer.java.payments.entities.paymentdetail.Status.AUTHORIZATIO
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
 @NoArgsConstructor
 @Getter
