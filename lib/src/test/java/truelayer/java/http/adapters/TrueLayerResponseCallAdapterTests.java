f4f00e07404309aef8d62b5f61fd884055538e71
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-01-19 19:25:33 +0100

    tests

---
+++ lib/src/test/java/truelayer/java/http/adapters/TrueLayerResponseCallAdapterTests.java
@@ -0,0 +1,10 @@
+package truelayer.java.http.adapters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+
+@Disabled
+class TrueLayerResponseCallAdapterTests {
+    // todo
+}
