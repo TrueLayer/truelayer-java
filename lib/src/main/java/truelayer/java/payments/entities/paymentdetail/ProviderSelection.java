ef0542c1a234e505724455fcddb92c4f6eba8110
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-01-21 20:53:18 +0100

    payment details bugfix

--- lib/src/main/java/truelayer/java/payments/entities/paymentdetail/ProviderSelection.java
+++ lib/src/main/java/truelayer/java/payments/entities/paymentdetail/ProviderSelection.java
@@ -3,10 +3,12 @@ package truelayer.java.payments.entities.paymentdetail;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.Value;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
+@ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProviderSelection extends BaseAuthorizationFlowAction {
     private String type = "provider_selection";
--- lib/src/main/java/truelayer/java/payments/entities/paymentdetail/Redirect.java
+++ lib/src/main/java/truelayer/java/payments/entities/paymentdetail/Redirect.java
@@ -2,10 +2,12 @@ package truelayer.java.payments.entities.paymentdetail;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.Value;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
+@ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Redirect extends BaseAuthorizationFlowAction {
     private final String type = "redirect";
