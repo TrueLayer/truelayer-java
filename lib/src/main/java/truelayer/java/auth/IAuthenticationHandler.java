f65d96784b9b1b1b4b77047b775da2d1e44c3e9c
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-02-09 10:33:55 +0100

    improved doc for auth handler

--- lib/src/main/java/truelayer/java/auth/IAuthenticationHandler.java
+++ lib/src/main/java/truelayer/java/auth/IAuthenticationHandler.java
@@ -7,7 +7,17 @@ import truelayer.java.http.entities.ApiResponse;
 
 /**
  * Exposes all the authentication related capabilities of the library.
+ *
+ * @see <a href="https://docs.truelayer.com/reference/generateaccesstoken"><i>Authentication</i> API reference</a>
  */
 public interface IAuthenticationHandler {
+
+    /**
+     * Gets an <i>OAuth</i> access token for the given scope(s).
+     *
+     * @param scopes a list of scopes. Might contain a single element.
+     * @return the response of the <i>Generate Access token</i> operation
+     * @see <a href="https://docs.truelayer.com/reference/generateaccesstoken"><i>Generate Access token</i> API reference</a>
+     */
     CompletableFuture<ApiResponse<AccessToken>> getOauthToken(List<String> scopes);
 }
