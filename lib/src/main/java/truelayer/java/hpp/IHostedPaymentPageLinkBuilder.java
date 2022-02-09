0ecb7ff4f4967d8dbc1ee598298a82bdc63c0f07
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-02-09 10:35:50 +0100

    improved doc for hpp handler

--- lib/src/main/java/truelayer/java/hpp/IHostedPaymentPageLinkBuilder.java
+++ lib/src/main/java/truelayer/java/hpp/IHostedPaymentPageLinkBuilder.java
@@ -12,6 +12,7 @@ public interface IHostedPaymentPageLinkBuilder {
      * @param resource_token the token of the payment created with a previous call to the Create Payment endpoint
      * @param returnUri the return URI where the client will be redirected once the payment is completed
      * @return the link to TrueLayer hosted payment page
+     * @see <a href="https://docs.truelayer.com/docs/hosted-payment-page">See <i>Hosted Payment Page</i> documentaion for further details</a>
      */
     URI getHostedPaymentPageLink(String paymentId, String resource_token, URI returnUri);
 }
