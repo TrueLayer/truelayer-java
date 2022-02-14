a4c05c655c62f4e3c55cca2bdef06e5e5322af1f
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-02-14 15:13:41 +0100

    documentation updates

--- lib/src/main/java/truelayer/java/Environment.java
+++ lib/src/main/java/truelayer/java/Environment.java
@@ -13,6 +13,10 @@ public class Environment {
 
     private final URI hppUri;
 
+    /**
+     * Builder for sandbox environment
+     * @return a sandbox environment object
+     */
     public static Environment sandbox() {
         return new Environment(
                 URI.create("https://auth.truelayer-sandbox.com"),
@@ -20,6 +24,10 @@ public class Environment {
                 URI.create("https://payment.truelayer-sandbox.com"));
     }
 
+    /**
+     * Builder for production environment
+     * @return a production environment object
+     */
     public static Environment live() {
         return new Environment(
                 URI.create("https://auth.truelayer.com"),
@@ -27,6 +35,13 @@ public class Environment {
                 URI.create("https://payment.truelayer.com"));
     }
 
+    /**
+     * Custom environment builder. Meant for testing purposes
+     * @param authApiUri the authentication API endpoint
+     * @param paymentsApiUri the Payments API endpoint
+     * @param hppUri the <i>Hosted Payment Page</i> endpoint
+     * @return a custom environment object
+     */
     public static Environment custom(URI authApiUri, URI paymentsApiUri, URI hppUri) {
         return new Environment(authApiUri, paymentsApiUri, hppUri);
     }
