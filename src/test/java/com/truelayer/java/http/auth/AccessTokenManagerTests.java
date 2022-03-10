afc4545775230e688cc91bd893589155d7807fe4
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-03-10 12:32:51 +0100

    stubbed test classes

---
+++ src/test/java/com/truelayer/java/http/auth/AccessTokenManagerTests.java
@@ -0,0 +1,7 @@
+package com.truelayer.java.http.auth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccessTokenManagerTests {
+    //todo
+}
---
+++ src/test/java/com/truelayer/java/integration/AccessTokenCacheTests.java
@@ -0,0 +1,5 @@
+package com.truelayer.java.integration;
+
+public class AccessTokenCacheTests {
+    //todo
+}
