ae4880056751121591b5909e8b78b72eae0789a2
Andrea Di Lisio <andrea.dilisio@truelayer.com>
2022-04-05 16:07:22 +0200

    beneficiary responses usability

--- src/test/java/com/truelayer/java/payments/entities/beneficiary/BeneficiaryTests.java
+++ src/test/java/com/truelayer/java/payments/entities/beneficiary/BeneficiaryTests.java
@@ -1,36 +1,65 @@
 package com.truelayer.java.payments.entities.beneficiary;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.truelayer.java.TrueLayerException;
 import com.truelayer.java.entities.beneficiary.Beneficiary;
 import com.truelayer.java.entities.beneficiary.ExternalAccount;
 import com.truelayer.java.entities.beneficiary.MerchantAccount;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BeneficiaryTests {
 
     @Test
-    @DisplayName("It should throw an exception if a user tries to access a merchant account as an external account")
-    public void shouldThrowAnExceptionIfMerchantIsAccessedAsExternalAccount() {
-        Beneficiary beneficiary = MerchantAccount.builder().build();
+    @DisplayName("It should yield true if instance is of type MerchantAccount")
+    public void shouldYieldTrueIfMerchantAccount() {
+        Beneficiary sut = MerchantAccount.merchantAccount().build();
 
-        Throwable thrown = Assertions.assertThrows(TrueLayerException.class, beneficiary::asExternalAccount);
-        assertEquals(
-                "beneficiary is of type MerchantAccount. Consider using asMerchantAccount() instead.",
-                thrown.getMessage());
+        assertTrue(sut.isMerchantAccount());
     }
 
     @Test
-    @DisplayName("It should throw an exception if a user tries to access an external account as a merchant account")
-    public void shouldThrowAnExceptionIfExternalIsAccessedAsMerchantAccount() {
-        Beneficiary beneficiary = ExternalAccount.builder().build();
+    @DisplayName("It should convert to an instance of class MerchantAccount")
+    public void shouldConvertToMerchantAccount() {
+        Beneficiary sut = MerchantAccount.merchantAccount().build();
 
-        Throwable thrown = Assertions.assertThrows(TrueLayerException.class, beneficiary::asMerchantAccount);
-        assertEquals(
-                "beneficiary is of type ExternalAccount. Consider using asExternalAccount() instead.",
-                thrown.getMessage());
+        assertDoesNotThrow(sut::asMerchantAccount);
+    }
+
+    @Test
+    @DisplayName("It should throw an error when converting to MerchantAccount")
+    public void shouldNotConvertToMerchantAccount() {
+        Beneficiary sut = ExternalAccount.builder().build();
+
+        Throwable thrown = assertThrows(TrueLayerException.class, sut::asMerchantAccount);
+
+        assertEquals(String.format("Beneficiary is of type %s.", sut.getClass().getSimpleName()), thrown.getMessage());
+    }
+
+    @Test
+    @DisplayName("It should yield true if instance is of type ExternalAccount")
+    public void shouldYieldTrueIfExternalAccount() {
+        Beneficiary sut = ExternalAccount.builder().build();
+
+        assertTrue(sut.isExternalAccount());
+    }
+
+    @Test
+    @DisplayName("It should convert to an instance of class ExternalAccount")
+    public void shouldConvertToExternalAccount() {
+        Beneficiary sut = ExternalAccount.builder().build();
+
+        assertDoesNotThrow(sut::asExternalAccount);
+    }
+
+    @Test
+    @DisplayName("It should throw an error when converting to ExternalAccount")
+    public void shouldNotConvertToExternalAccount() {
+        Beneficiary sut = MerchantAccount.builder().build();
+
+        Throwable thrown = assertThrows(TrueLayerException.class, sut::asExternalAccount);
+
+        assertEquals(String.format("Beneficiary is of type %s.", sut.getClass().getSimpleName()), thrown.getMessage());
     }
 }
