265c04ae9b32469004245cb0d733f465d255339c
dili91 <andrea.dilisio91@gmail.com>
2022-05-05 17:06:01 +0200

    testing sync flow

--- src/main/java/com/truelayer/java/http/auth/AccessTokenManager.java
+++ src/main/java/com/truelayer/java/http/auth/AccessTokenManager.java
@@ -36,7 +36,7 @@ public class AccessTokenManager implements IAccessTokenManager {
             });
         }
 
-        return tryGetTokenAsync();
+        return tryGetToken();
     }
 
     @Override
