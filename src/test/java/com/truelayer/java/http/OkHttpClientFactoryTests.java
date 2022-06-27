a8b4694f2a148d75dec09b7602c7b3bf5200e680
dili91 <andrea.dilisio91@gmail.com>
2022-06-27 19:35:30 +0200

    linting

--- src/test/java/com/truelayer/java/http/OkHttpClientFactoryTests.java
+++ src/test/java/com/truelayer/java/http/OkHttpClientFactoryTests.java
@@ -1,49 +1,44 @@
 package com.truelayer.java.http;
 
-import com.truelayer.java.ConnectionPoolOptions;
-import com.truelayer.java.TestUtils;
-import com.truelayer.java.versioninfo.VersionInfo;
-import com.truelayer.java.versioninfo.VersionInfoLoader;
-import okhttp3.OkHttpClient;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import com.truelayer.java.TestUtils;
+import com.truelayer.java.versioninfo.VersionInfo;
+import com.truelayer.java.versioninfo.VersionInfoLoader;
+import java.time.Duration;
+import okhttp3.OkHttpClient;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class OkHttpClientFactoryTests {
 
     @Test
     @DisplayName("It should build an Auth API client")
-    public void shouldCreateAnAuthApiClient(){
+    public void shouldCreateAnAuthApiClient() {
         OkHttpClientFactory sut = getOkHttpClientFactory();
 
-        OkHttpClient authClient = sut.buildAuthApiClient(TestUtils.getClientCredentials(), Duration.ofSeconds(1), null, null, null);
+        OkHttpClient authClient =
+                sut.buildAuthApiClient(TestUtils.getClientCredentials(), Duration.ofSeconds(1), null, null, null);
 
-        //todo: improve assertions and pass non null values above
+        // todo: improve assertions and pass non null values above
         assertNotNull(authClient);
         assertFalse(authClient.interceptors().isEmpty());
     }
 
     @Test
     @Disabled
-    public void shouldThrowCredentialsMissingException(){}
+    public void shouldThrowCredentialsMissingException() {}
 
-    private OkHttpClientFactory getOkHttpClientFactory(){
+    private OkHttpClientFactory getOkHttpClientFactory() {
         VersionInfo versionInfo = VersionInfo.builder()
                 .libraryName("truelayer-java")
                 .libraryVersion("1.0.0")
                 .build();
         VersionInfoLoader versionInfoLoader = mock(VersionInfoLoader.class);
-        when(versionInfoLoader.load())
-                .thenReturn(versionInfo);
+        when(versionInfoLoader.load()).thenReturn(versionInfo);
 
         return new OkHttpClientFactory(versionInfoLoader);
     }
