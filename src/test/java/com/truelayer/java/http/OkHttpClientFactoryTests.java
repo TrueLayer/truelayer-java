15fb631a258cd9585e5f7b646433030148a0d213
dili91 <andrea.dilisio91@gmail.com>
2022-07-05 17:13:55 +0200

    linting

--- src/test/java/com/truelayer/java/http/OkHttpClientFactoryTests.java
+++ src/test/java/com/truelayer/java/http/OkHttpClientFactoryTests.java
@@ -1,7 +1,6 @@
 package com.truelayer.java.http;
 
 import static com.truelayer.java.TestUtils.getClientCredentials;
-import static com.truelayer.java.TestUtils.getTestEnvironment;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -11,7 +10,6 @@ import com.truelayer.java.TestUtils;
 import com.truelayer.java.TrueLayerException;
 import com.truelayer.java.auth.AuthenticationHandler;
 import com.truelayer.java.auth.IAuthenticationHandler;
-import com.truelayer.java.http.auth.AccessTokenInvalidator;
 import com.truelayer.java.http.auth.cache.SimpleCredentialsCache;
 import com.truelayer.java.http.interceptors.AuthenticationInterceptor;
 import com.truelayer.java.http.interceptors.IdempotencyKeyInterceptor;
@@ -20,7 +18,6 @@ import com.truelayer.java.http.interceptors.UserAgentInterceptor;
 import com.truelayer.java.http.interceptors.logging.HttpLoggingInterceptor;
 import com.truelayer.java.versioninfo.VersionInfo;
 import com.truelayer.java.versioninfo.VersionInfoLoader;
-
 import java.net.URI;
 import java.time.Clock;
 import java.time.Duration;
@@ -66,26 +63,22 @@ class OkHttpClientFactoryTests {
     @Test
     @DisplayName("It should build a Payments API client")
     public void shouldThrowCredentialsMissingException() {
-        OkHttpClient authClient = getOkHttpClientFactory()
-                .buildAuthApiClient(
-                        getClientCredentials(),
-                        null,
-                        null,
-                        null,
-                        null);
+        OkHttpClient authClient =
+                getOkHttpClientFactory().buildAuthApiClient(getClientCredentials(), null, null, null, null);
         IAuthenticationHandler authenticationHandler = AuthenticationHandler.New()
                 .clientCredentials(getClientCredentials())
                 .httpClient(RetrofitFactory.build(authClient, URI.create("http://localhost")))
                 .build();
 
         OkHttpClient paymentClient = getOkHttpClientFactory()
-                .buildPaymentsApiClient(authClient, authenticationHandler,
+                .buildPaymentsApiClient(
+                        authClient,
+                        authenticationHandler,
                         TestUtils.getSigningOptions(),
                         new SimpleCredentialsCache(Clock.systemUTC()));
 
         assertTrue(
-                paymentClient.interceptors().stream()
-                        .anyMatch(i -> i.getClass().equals(SignatureInterceptor.class)),
+                paymentClient.interceptors().stream().anyMatch(i -> i.getClass().equals(SignatureInterceptor.class)),
                 "Signature interceptor not found");
         assertTrue(
                 paymentClient.interceptors().stream()
@@ -97,20 +90,11 @@ class OkHttpClientFactoryTests {
     @Test
     @DisplayName("It should throw an exception if signing options are missing")
     public void shouldThrowSigningOptionsMissingException() {
-        OkHttpClient authClient = getOkHttpClientFactory()
-                .buildAuthApiClient(
-                        getClientCredentials(),
-                        null,
-                        null,
-                        null,
-                        null);
+        OkHttpClient authClient =
+                getOkHttpClientFactory().buildAuthApiClient(getClientCredentials(), null, null, null, null);
 
         Throwable thrown = assertThrows(TrueLayerException.class, () -> getOkHttpClientFactory()
-                .buildPaymentsApiClient(
-                        authClient,
-                        null,
-                        null,
-                        null));
+                .buildPaymentsApiClient(authClient, null, null, null));
 
         assertEquals("signing options must be set", thrown.getMessage());
     }
