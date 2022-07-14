1c6772cd1ba10530efe3380b611f827c03c8b4c0
dili91 <andrea.dilisio91@gmail.com>
2022-07-14 15:26:27 +0200

    constant class defined

---
+++ src/test/java/com/truelayer/java/contract/Constant.java
@@ -0,0 +1,16 @@
+package com.truelayer.java.contract;
+
+public final class Constant {
+    // Matchers
+    public static final String UUID_REGEX = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+    public static final String JWT_TOKEN_REGEX = "[a-zA-Z0-9_-]*.[a-zA-Z0-9_-]*.[a-zA-Z0-9_-]*";
+    public static final String PAYMENT_STATUS_REGEX =
+            "^(authorization_required|authorizing|authorized|executed|settled|failed)$";
+
+    // Samples
+    public static final String A_JWT_TOKEN =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    public static final String A_PAYMENT_ID = "48c890dc-8c03-428c-9a8b-2f383fd0ba38";
+    public static final String MERCHANT_ACCOUNT_ID = "B7DAEA71-592E-486B-9C00-7132D1FD7AD1";
+    public static final String RETURN_URI = "http://localhost:8080/callback";
+}
--- src/test/java/com/truelayer/java/contract/PaymentsContractTests.java
+++ src/test/java/com/truelayer/java/contract/PaymentsContractTests.java
@@ -1,6 +1,7 @@
 package com.truelayer.java.contract;
 
 import static com.truelayer.java.TestUtils.assertNotError;
+import static com.truelayer.java.contract.Constant.*;
 
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
@@ -25,20 +26,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PaymentsContractTests extends ContractTests {
-
-    // Matchers
-    public static final String UUID_REGEX = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
-    public static final String JWT_TOKEN_REGEX = "[a-zA-Z0-9_-]*.[a-zA-Z0-9_-]*.[a-zA-Z0-9_-]*";
-    public static final String PAYMENT_STATUS_REGEX =
-            "^(authorization_required|authorizing|authorized|executed|settled|failed)$";
-
-    // Samples
-    public static final String A_JWT_TOKEN =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-    public static final String A_PAYMENT_ID = "48c890dc-8c03-428c-9a8b-2f383fd0ba38";
-    public static final String MERCHANT_ACCOUNT_ID = "B7DAEA71-592E-486B-9C00-7132D1FD7AD1";
-    public static final String RETURN_URI = "http://localhost:8080/callback";
-
     @SneakyThrows
     @Pact(consumer = "JavaSDK", provider = "PaymentsV3")
     RequestResponsePact createAndAuthorizePayment(PactDslWithProvider builder) {
