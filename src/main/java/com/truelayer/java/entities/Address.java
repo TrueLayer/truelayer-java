1bea7f89d2098ef587ccd594dfabab4a542609c0
tl-wajid-malik <61701789+tl-wajid-malik@users.noreply.github.com>
2022-09-13 17:24:36 +0100

    Add extra fields to user object

---
+++ src/main/java/com/truelayer/java/entities/Address.java
@@ -0,0 +1,28 @@
+package com.truelayer.java.entities;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import java.util.Optional;
+import lombok.*;
+
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Address {
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private String countryCode;
+
+    @JsonGetter
+    private Optional<String> getAddressLine2() {
+        return Optional.ofNullable(addressLine2);
+    }
+}
