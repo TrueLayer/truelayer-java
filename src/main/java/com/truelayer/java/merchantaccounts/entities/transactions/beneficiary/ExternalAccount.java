ede7ff0b972edad25f2c050979d1ddaf2ba8315b
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2023-01-13 18:21:27 +0100

    [EWT-85] fixes existing items returned by the `merchant-accounts/{id}/transactions` endpoint and add support for refunds (#180)

---
+++ src/main/java/com/truelayer/java/merchantaccounts/entities/transactions/beneficiary/ExternalAccount.java
@@ -0,0 +1,20 @@
+package com.truelayer.java.merchantaccounts.entities.transactions.beneficiary;
+
+import static com.truelayer.java.merchantaccounts.entities.transactions.beneficiary.Beneficiary.Type.EXTERNAL_ACCOUNT;
+
+import com.truelayer.java.merchantaccounts.entities.transactions.accountidentifier.AccountIdentifier;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class ExternalAccount extends Beneficiary {
+    private final Type type = EXTERNAL_ACCOUNT;
+
+    private String reference;
+
+    private String accountHolderName;
+
+    private List<AccountIdentifier> accountIdentifiers;
+}
