08c086b0c556f84757ebc5d1f60229d3e0a8d35a
tl-luca-baggi <luca.baggi@truelayer.com>
2023-05-09 11:05:40 +0200

    Add unit tests for TL Client builder

--- src/test/java/com/truelayer/java/TrueLayerClientBuilderTests.java
+++ src/test/java/com/truelayer/java/TrueLayerClientBuilderTests.java
@@ -95,4 +95,18 @@ class TrueLayerClientBuilderTests {
                 "mandates handler not initialized. Make sure you specified the required signing options while initializing the library",
                 thrown.getMessage());
     }
+
+    @Test
+    @DisplayName(
+            "It should throw an initialization error when invoking the payouts handler and signing options are missing")
+    public void itShouldThrowIfPayoutsHandlerIsInvokedWithoutSigningOptions() {
+        ITrueLayerClient trueLayerClient =
+                TrueLayerClient.New().clientCredentials(getClientCredentials()).build();
+
+        Throwable thrown = assertThrows(TrueLayerException.class, trueLayerClient::payouts);
+
+        assertEquals(
+                "payouts handler not initialized. Make sure you specified the required signing options while initializing the library",
+                thrown.getMessage());
+    }
 }
