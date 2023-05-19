24db498c3dfa99ff483f02393bf1cecf914a8c86
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2023-05-19 10:32:26 +0200

    cleanup

--- src/test/java/com/truelayer/java/http/interceptors/BaseInterceptorTests.java
+++ src/test/java/com/truelayer/java/http/interceptors/BaseInterceptorTests.java
@@ -2,14 +2,11 @@ package com.truelayer.java.http.interceptors;
 
 import static org.mockito.Mockito.*;
 
-import com.truelayer.java.Constants;
-import com.truelayer.java.entities.IdempotencyKey;
 import java.util.function.Consumer;
 import lombok.SneakyThrows;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
-import org.apache.commons.lang3.ObjectUtils;
 import org.mockito.ArgumentCaptor;
 
 public abstract class BaseInterceptorTests {
@@ -18,18 +15,10 @@ public abstract class BaseInterceptorTests {
     protected abstract Interceptor getInterceptor();
 
     protected void buildRequest() {
-        buildRequest(null);
-    }
-
-    protected void buildRequest(IdempotencyKey idempotencyKey) {
-        Request.Builder requestBuilder = new Request.Builder().url(HttpUrl.get("http://localhost"));
-
-        if (ObjectUtils.isNotEmpty(idempotencyKey)) {
-            requestBuilder.addHeader(Constants.HeaderNames.IDEMPOTENCY_KEY, idempotencyKey.getValue());
-        }
-
+        Request request =
+                new Request.Builder().url(HttpUrl.get("http://localhost")).build();
         chain = mock(Interceptor.Chain.class);
-        when(chain.request()).thenReturn(requestBuilder.build());
+        when(chain.request()).thenReturn(request);
     }
 
     @SneakyThrows
