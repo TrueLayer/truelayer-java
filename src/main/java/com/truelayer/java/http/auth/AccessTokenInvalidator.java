e13858ebbb017b2c9403fe7a711cd6ca115f125c
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2023-08-17 16:06:21 +0200

    added todo

--- src/main/java/com/truelayer/java/http/auth/AccessTokenInvalidator.java
+++ src/main/java/com/truelayer/java/http/auth/AccessTokenInvalidator.java
@@ -16,7 +16,8 @@ public class AccessTokenInvalidator implements Authenticator {
     private final IAccessTokenManager tokenManager;
 
     @Override
-    public Request authenticate(Route route, Response response) throws IOException {
+    public Request authenticate(Route route, Response response) {
+        //TODO: can we invalidate a specific token in memory as opposed to all ?
         if (isNotEmpty(response.request().header(AUTHORIZATION))) {
             // we are here in case of 401 on requests containing an Authorization HTTP header
             tokenManager.invalidateToken();
