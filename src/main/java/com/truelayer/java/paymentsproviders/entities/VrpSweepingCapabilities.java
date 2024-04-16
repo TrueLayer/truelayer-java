f19987e44df6882d0a81c08870da35e37bc893d6
tl-luca-baggi <luca.baggi@truelayer.com>
2024-04-16 17:48:42 +0200

    add provider availabilty to VRP capabilities for payments provider endpoint

--- src/main/java/com/truelayer/java/paymentsproviders/entities/VrpSweepingCapabilities.java
+++ src/main/java/com/truelayer/java/paymentsproviders/entities/VrpSweepingCapabilities.java
@@ -1,9 +1,12 @@
 package com.truelayer.java.paymentsproviders.entities;
 
+import com.truelayer.java.entities.ProviderAvailability;
 import com.truelayer.java.payments.entities.ReleaseChannel;
 import lombok.Value;
 
 @Value
 public class VrpSweepingCapabilities {
     ReleaseChannel releaseChannel;
+
+    ProviderAvailability availability;
 }
