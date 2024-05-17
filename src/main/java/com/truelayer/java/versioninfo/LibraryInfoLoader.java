90273cad8b74bb4129097af0de7caf9c7a875591
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-05-17 17:10:43 +0200

    Adjust settlement/authorisation timeouts in acceptance tests, and review version info loader implementation (#289)

--- src/main/java/com/truelayer/java/versioninfo/LibraryInfoLoader.java
+++ src/main/java/com/truelayer/java/versioninfo/LibraryInfoLoader.java
@@ -1,10 +1,13 @@
 package com.truelayer.java.versioninfo;
 
+import static java.util.Objects.requireNonNull;
+
 import com.truelayer.java.Constants;
 import com.truelayer.java.TrueLayerException;
-import org.apache.commons.configuration2.PropertiesConfiguration;
-import org.apache.commons.configuration2.builder.fluent.Configurations;
-import org.apache.commons.configuration2.ex.ConfigurationException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  * Class the loads the version of the library during the client initialization.
@@ -15,18 +18,23 @@ public class LibraryInfoLoader {
     private static final String CONFIG_FILE_PREXIF = "truelayer-java";
 
     public VersionInfo load() {
-        PropertiesConfiguration libraryVersionProps = getVersionInfoPropertiesFile();
+        Properties libraryVersionProps = getVersionInfoProperties();
 
         return VersionInfo.builder()
-                .libraryName(libraryVersionProps.getString(Constants.VersionInfo.NAME))
-                .libraryVersion(libraryVersionProps.getString(Constants.VersionInfo.VERSION))
+                .libraryName(libraryVersionProps.getProperty(Constants.VersionInfo.NAME))
+                .libraryVersion(libraryVersionProps.getProperty(Constants.VersionInfo.VERSION))
                 .build();
     }
 
-    private PropertiesConfiguration getVersionInfoPropertiesFile() {
+    private Properties getVersionInfoProperties() {
         try {
-            return new Configurations().properties(CONFIG_FILE_PREXIF + "." + "version" + ".properties");
-        } catch (ConfigurationException e) {
+            Properties versionInfoProps = new Properties();
+            versionInfoProps.load(Files.newInputStream(Paths.get(requireNonNull(getClass()
+                            .getClassLoader()
+                            .getResource(CONFIG_FILE_PREXIF + "." + "version" + ".properties"))
+                    .getPath())));
+            return versionInfoProps;
+        } catch (IOException e) {
             throw new TrueLayerException("Unable to load library version file", e);
         }
     }
