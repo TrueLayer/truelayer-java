ae59b8491961a2d889a109311f6ba40f887e21ae
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2024-06-13 16:56:29 +0200

    [EWT-567] Version info management simplification (#293)

--- src/main/java/com/truelayer/java/versioninfo/LibraryInfoLoader.java
+++ src/main/java/com/truelayer/java/versioninfo/LibraryInfoLoader.java
@@ -1,18 +1,12 @@
 package com.truelayer.java.versioninfo;
 
-import static java.util.Objects.requireNonNull;
-
 import com.truelayer.java.Constants;
 import com.truelayer.java.TrueLayerException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
  * Class the loads the version of the library during the client initialization.
- * This component leverages a gradle build steps that prepares a truelayer-java.version.properties
- * file in project's resources directory.
  */
 public class LibraryInfoLoader {
     private static final String CONFIG_FILE_PREXIF = "truelayer-java";
@@ -29,10 +23,9 @@ public class LibraryInfoLoader {
     private Properties getVersionInfoProperties() {
         try {
             Properties versionInfoProps = new Properties();
-            versionInfoProps.load(Files.newInputStream(Paths.get(requireNonNull(getClass()
-                            .getClassLoader()
-                            .getResource(CONFIG_FILE_PREXIF + "." + "version" + ".properties"))
-                    .getPath())));
+            versionInfoProps.load(getClass()
+                    .getClassLoader()
+                    .getResourceAsStream(CONFIG_FILE_PREXIF + "." + "version" + ".properties"));
             return versionInfoProps;
         } catch (IOException e) {
             throw new TrueLayerException("Unable to load library version file", e);
